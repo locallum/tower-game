138abdc60a82c57413c0de12440582d3580b9927
Callum <105720890+locallum@users.noreply.github.com>
2024-05-17 02:51:31 +1000

    Added Tower placing
    Made grid recognize hover and clicking, placing the default tower on click

--- TowerGame/Assets/Health.cs
+++ TowerGame/Assets/Health.cs
@@ -8,11 +8,14 @@ public class Health : MonoBehaviour {
     [Header("Attributes")]
     [SerializeField] private int hitPoints = 3;
 
+    private bool isDestroyed = false;
+
     public void TakeDamage(int dmg) {
         hitPoints -= dmg;
 
-        if (hitPoints <= 0) {
+        if (hitPoints <= 0 && !isDestroyed) {
             EnemySpawner.onEnemyDestroy.Invoke();
+            isDestroyed = true;
             Destroy(gameObject);
         }
     }
